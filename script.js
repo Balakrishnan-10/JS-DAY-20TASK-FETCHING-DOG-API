d95bf520a1682efb6dbb99f52a14b5217bdba765
Balakrishnan-10 <krishnabala1495@gmail.com>
2024-10-12 12:22:35 +0530

    Using Fetching - Dog API

---
+++ script.js
@@ -0,0 +1,31 @@
+//adding the eventlistener to the button to  display the details
+
+const button = document.querySelector(".btn");
+button.addEventListener("click", () => {
+  displayfact();
+});
+
+//fact displayed by fetched api
+function displayfact() {
+  const url = fetch("https://dogapi.dog/api/v2/breeds?page[number]=29");
+  url
+    .then((data) => data.json()) //convert to json format and to object
+    .then((result) => {
+      const objectData = result.data;
+      let tableData = "";
+      //displayed the data in the table
+      objectData.map((values) => {
+        tableData += `<tr>
+        <td>${values.type}</td>
+        <td>${values.attributes.name}</td>
+        <td>${values.attributes.description}</td>
+        <td>Max :${values.attributes.life.max}, Min :${values.attributes.life.min}</td>
+        </tr>
+        
+        `;
+      });
+
+      document.querySelector(".tbody").innerHTML = tableData;
+    })
+    .catch((err) => console.log(err));
+}
